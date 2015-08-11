2c5b3e0cda0d6713eb3ca26b6537fe48ad408707
gbrock <contact@gbrock.com>
2015-08-11 17:55:50 -0400

    Loading Laravel

---
+++ src/Providers/PartyServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GridPrinciples\Party\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class PartyServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GridPrinciples\Party\Tests;
+
+use GridPrinciples\Party\Providers\PartyServiceProvider;
+use Illuminate\Filesystem\ClassFinder;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+abstract class TestCase extends BaseTestCase
+{
+    /**
+     * Boots the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__ . '/../vendor/laravel/laravel/bootstrap/app.php';
+
+        // Register our package's service provider
+        $app->register(PartyServiceProvider::class);
+
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+
+        return $app;
+    }
+}
