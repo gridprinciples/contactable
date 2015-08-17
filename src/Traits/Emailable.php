98abe1c8d5f0f795cd84fd453f5b5915117095e2
gbrock <contact@gbrock.com>
2015-08-17 10:07:51 -0400

    Removed "settable field" functionality from Emailable trait; it was confusing

--- src/Traits/Emailable.php
+++ src/Traits/Emailable.php
@@ -7,18 +7,6 @@ use Illuminate\Support\Collection;
 
 trait Emailable
 {
-    protected $emailsToSave;
-
-    /**
-     * Load this trait.
-     */
-    public static function bootEmailable()
-    {
-        self::saved(function ($model) {
-            $model->applyRelatedEmails();
-        });
-    }
-
     /**
      * The relationship to other models.
      *
@@ -27,87 +15,5 @@ trait Emailable
     public function emails()
     {
         return $this->morphMany(EmailAddress::class, 'emailable');
-    }
-
-    /**
-     * Mutator for the e-mail address "field", really a relationship.
-     *
-     * @param $emails
-     */
-    public function setEmailAttribute($emails)
-    {
-        if ($emails === false) {
-            $this->deleteRelatedEmailsOnSave();
-
-            return;
-        }
-
-        if (is_string($emails)) {
-            // Only one e-mail was passed as a string
-            $emails = [$emails];
-        }
-
-        if (!is_object($emails) || !class_basename($emails) === 'Collection') {
-            // Collect the values into a Collection
-            $emails = collect((array)$emails);
-        }
-
-        $this->saveEmails($emails);
-    }
-
-    /**
-     * Given a Collection, save as the current e-mails.
-     *
-     * @param $emails Collection
-     * @return $this
-     */
-    protected function saveEmails(Collection $emails)
-    {
-        foreach ($emails as $k => $address) {
-            if (is_object($address) && get_class($address) === EmailAddress::class) {
-                // This is already an e-mail address object.
-                continue;
-            }
-
-            $emails[$k] = new EmailAddress(['address' => $address]);
-        }
-
-        if (!$this->exists) {
-            // Record doesn't exist, so defer until it's saved.
-            $this->emailsToSave = $emails;
-        } else {
-            foreach ($emails as $email) {
-                $email->emailable()->associate($this);
-                $email->save();
-            }
-        }
-
-        return;
-    }
-
-    /**
-     * Delete all associated e-mails, on save.
-     * Doesn't actually do anything.  Defers until the "save" method is run.
-     */
-    protected function deleteRelatedEmailsOnSave()
-    {
-        $this->emailsToSave = false;
-    }
-
-    /**
-     * Save any cached "stragglers".
-     */
-    protected function applyRelatedEmails()
-    {
-        if ($this->emailsToSave) {
-            $this->saveEmails($this->emailsToSave);
-        } elseif ($this->emailsToSave === false) {
-            $this->deleteEmails();
-        }
-    }
-
-    protected function deleteEmails()
-    {
-        $this->emails()->delete();
     }
 }
--- tests/EmailAssociation.php
+++ tests/EmailAssociation.php
@@ -2,16 +2,15 @@
 
 namespace GridPrinciples\Party\Tests;
 
+use GridPrinciples\Party\EmailAddress;
 use GridPrinciples\Party\Tests\Cases\UserTestCase;
-use GridPrinciples\Party\Tests\Mocks\User;
 
 class EmailAssociation extends UserTestCase
 {
-    protected $mockUserClass = User::class;
-
     public function test_user_can_associate_one_email_address()
     {
-        $user = $this->createUser(['email' => 'me@example.com']);
+        $user = $this->createUser();
+        $user->emails()->save(new EmailAddress(['address' => 'me@example.com']));
 
         $this->assertCount(1, $user->emails);
         $this->assertContains('me@example.com', $user->emails->lists('address'));
@@ -19,7 +18,11 @@ class EmailAssociation extends UserTestCase
 
     public function test_user_can_associate_multiple_email_addresses()
     {
-        $user = $this->createUser(['email' => ['multi1@example.com', 'multi2@example.com']]);
+        $user = $this->createUser();
+        $user->emails()->saveMany([
+            new EmailAddress(['address' => 'multi1@example.com']),
+            new EmailAddress(['address' => 'multi2@example.com']),
+        ]);
 
         $this->assertCount(2, $user->emails);
         $this->assertContains('multi1@example.com', $user->emails->lists('address'));
@@ -28,10 +31,9 @@ class EmailAssociation extends UserTestCase
 
     public function test_user_can_delete_email_addresses()
     {
-        $user = $this->createUser(['email' => 'delete_immediately@example.com']);
-
-        $user->email = false;
-        $user->save();
+        $user = $this->createUser();
+        $user->emails()->save(new EmailAddress(['address' => 'delete_immediately@example.com']));
+        $user->emails()->delete();
 
         $this->assertCount(0, $user->emails);
     }
